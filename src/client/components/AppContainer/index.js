5183748912c97dd495898d347919cd2836d0fd56
chuk-chuk <yulia.v.litvin@gmail.com>
2021-07-16 14:29:19 +0200

    Extract the menuPreview into its own component

--- src/client/components/AppContainer/index.js
+++ src/client/components/AppContainer/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import MenuProvider from '../../menuProvider';
 import Header from '../Header';
 import ItemList from '../ItemList';
+import MenuPreview from '../MenuPreview';
 import '../../App.css';
 
 export default function AppContainer() {
@@ -36,45 +37,7 @@ export default function AppContainer() {
               <ItemList />
             </div>
             <div className="col-8">
-              <h2>Menu preview</h2>
-              <ul className="menu-preview">
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                  <button className="remove-item">x</button>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                  <button className="remove-item">x</button>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                  <button className="remove-item">x</button>
-                </li>
-                <li className="item">
-                  <h2>Dummy item</h2>
-                  <p>
-                    <span className="dietary">ve</span>
-                    <span className="dietary">v</span>
-                    <span className="dietary">n!</span>
-                  </p>
-                  <button className="remove-item">x</button>
-                </li>
-              </ul>
+            <MenuPreview />
             </div>
           </div>
         </div>
