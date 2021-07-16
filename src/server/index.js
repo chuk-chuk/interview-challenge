7e6da6989cfb327562b2715ea8c9de6dadff9583
chuk-chuk <yulia.v.litvin@gmail.com>
2021-07-16 19:03:23 +0200

    Add new endpoints to handle select and delete of the items

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const items = require('./items');
+const bodyParser = require('body-parser'); 
 
 const app = express();
+app.use(bodyParser.json()); 
 
 const port = process.env.PORT || 8080;
+
+const selectedList = []
 
 app.use(express.static('dist'));
 
@@ -16,6 +20,23 @@ app.get('/api/items', (req, res) => {
   } else {
     res.send({ items })
   }
+});
+
+app.get('/api/selectedItems', (req, res) => {
+  const filteredList = selectedList.filter(item => !selectedList.includes(item.id))
+  res.send({ items: filteredList })
+});
+
+app.post('/api/selectedItems', (req, res) => {
+  var itemToAdd = req.body;
+  selectedList.push(itemToAdd);
+  res.send("Item added to preview list!");
+});
+
+app.delete('/api/selectedItems/:id', (req, res) => {
+  const { id } = req.params;
+  selectedList.filter(item => item.id !== id)
+  res.send("Item removed!");
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
